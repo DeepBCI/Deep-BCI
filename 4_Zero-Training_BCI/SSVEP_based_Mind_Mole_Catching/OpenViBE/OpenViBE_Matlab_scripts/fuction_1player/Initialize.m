622ac1ac59b2cbc62d5d84bf8d7dce8adcad415a
HeegyuKim2019 <56532590+HeegyuKim2019@users.noreply.github.com>
2020-03-11 15:56:26 +0900

    Update Initialize.m

--- 4_Zero-Training_BCI/SSVEP_based_Mind_Mole_Catching/OpenViBE/OpenViBE_Matlab_scripts/fuction_1player/Initialize.m
+++ 4_Zero-Training_BCI/SSVEP_based_Mind_Mole_Catching/OpenViBE/OpenViBE_Matlab_scripts/fuction_1player/Initialize.m
@@ -1,26 +1,13 @@
 function box_out = Initialize(box_in)
-% Initialization
 
+    % Initialization
     box_in.user_data.time = 3;
     box_in.user_data.freq = [10, 12, 15, 7];
     box_in.user_data.marker  = {'1','up';'2', 'left';'3', 'right';'4', 'down'};
     box_in.user_data.interval = [0 3000];  % ms
-%     box_in.user_data.fs = 300; %box_in.inputs{1}.header.sampling_rate;
     box_in.user_data.m_cnt = 0;
 
-
-%     for i=1:size(box_in.settings,2)
-% %        fprintf('\t%s : %s\n',box_in.settings(i).name, num2str(box_in.settings(i).value));
-%           disp('\t%s : %s\n',box_in.settings(i).name, num2str(box_in.settings(i).value)')
-%     end
-    %  %%%%% eeglab 
-%      cd('C:/Users/Bio_lab_HG/Documents/MATLAB/fieldtrip-20190618'); % FieldTrip
-%      ft_defaults;    
-%      cd('C:/Users/Bio_lab_HG/Documents/MATLAB/eeglab2019_0'); % EEGLAB
-%      close all; % close EEGLAB GUI
-     
-     disp('Loading DSI chanlocs..');
-%     
+    disp('Loading DSI chanlocs..');  
     box_in.user_data.is_headerset = false;
 	
 	% We also add some statistics
@@ -29,4 +16,3 @@ function box_out = Initialize(box_in)
     
     box_out = box_in;
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
