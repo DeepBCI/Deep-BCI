14e672f9a52229ce2c3611b1d10707b66acc7542
Sunhee Hwang <sunny16@yonsei.ac.kr>
2020-05-20 16:38:35 +0900

    Update dataset.py description

--- 1_Intelligent_BCI/CNN_Based_Driver_Vigilance_Estimation/dataloader.py
+++ 1_Intelligent_BCI/CNN_Based_Driver_Vigilance_Estimation/dataloader.py
@@ -10,18 +10,19 @@ import pdb
 import numpy as np
 
 class dataloader(data.Dataset):
+	
 	def __init__(self, data_name='1_20151124_noon_2.npy', d_type='train', data_dir = '/mnt/dataset/eeg/seedvig/DE_LDS_1/'):
 		if d_type == 'train':
-			dx = 'tr_'
-			dy = 'try_'
+			dx = 'tr_' # training eeg
+			dy = 'try_' # training eeg labels
 		else:
-			dx = 'te_'
-			dy = 'tey_'
+			dx = 'te_' # test eeg
+			dy = 'tey_' # test eeg labels
 
 		self.x = torch.from_numpy(np.load(data_dir+dx+data_name)).float()
 		self.y = torch.from_numpy(np.load(data_dir+dy+data_name)).float()
 
-		self.l = int(self.x.shape[0])
+		self.l = int(self.x.shape[0]) # number of samples (train/test)
 
 
 	def __len__(self):
