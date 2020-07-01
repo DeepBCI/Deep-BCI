62b90f4c17c2b25ead7dd05e070928755e8b7bff
HeegyuKim2019 <56532590+HeegyuKim2019@users.noreply.github.com>
2020-07-01 16:06:16 +0900

    fixed bugs
    the variable of data time has some minor bug, so fixed it

--- 4_Zero-Training_BCI/SSVEP_based_BCI_speller/Matlab_scripts/Initialize.m
+++ 4_Zero-Training_BCI/SSVEP_based_BCI_speller/Matlab_scripts/Initialize.m
@@ -1,10 +1,10 @@
 function box_out = Initialize(box_in)
 % Initialization
-
-    box_in.user_data.time = 3.0;
+    data_length = 3.0;
+    box_in.user_data.time = data_length;
     box_in.user_data.freq =  [14 15 16 17 18 19 20 21 22];
     box_in.user_data.marker  = {'1','up';'2', 'left';'3', 'right';'4', 'down'};
-    box_in.user_data.interval = [0 3000];  % ms
+    box_in.user_data.interval = [0 data_length*1000];  % ms
 %     box_in.user_data.fs = 300; %box_in.inputs{1}.header.sampling_rate;
     box_in.user_data.m_cnt = 0;
 
