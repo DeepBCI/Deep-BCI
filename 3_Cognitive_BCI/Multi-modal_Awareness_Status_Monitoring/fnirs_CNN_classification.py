94ac6d522ce7fab43bca1cd8210cc16fed853873
rock <choimosi@google.com>
2020-10-31 15:14:56 +0900

    train and make plot

--- 3_Cognitive_BCI/Multi-modal_Awareness_Status_Monitoring/fnirs_CNN_classification.py
+++ 3_Cognitive_BCI/Multi-modal_Awareness_Status_Monitoring/fnirs_CNN_classification.py
@@ -46,3 +46,31 @@ model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(3, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# 3. 모델 학습시키기
+hist = model.fit(x_train, y_train, epochs=20, batch_size=40, validation_data=(x_val, y_val))
+
+# 4. 학습과정 살펴보기
+%matplotlib inline
+import matplotlib.pyplot as plt
+
+fig, loss_ax = plt.subplots()
+
+acc_ax = loss_ax.twinx()
+
+loss_ax.plot(hist.history['loss'], 'y', label='train loss')
+loss_ax.plot(hist.history['val_loss'], 'r', label='val loss')
+loss_ax.set_ylim([0.0, 1])
+
+acc_ax.plot(hist.history['acc'], 'b', label='train acc')
+acc_ax.plot(hist.history['val_acc'], 'g', label='val acc')
+acc_ax.set_ylim([0.0, 1.0])
+
+loss_ax.set_xlabel('epoch')
+loss_ax.set_ylabel('loss')
+acc_ax.set_ylabel('accuray')
+
+loss_ax.legend(loc='upper left')
+acc_ax.legend(loc='lower left')
+
+plt.show()
