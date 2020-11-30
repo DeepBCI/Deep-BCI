68e442c6424a605ac6b380e5b3e0d118062f4ca1
rock <chomosi@gmail.com>
2020-12-01 00:00:30 +0900

    add data search

--- 3_Cognitive_BCI/Multi-modal_Awareness_Status_Monitoring/BCI_generator.py
+++ 3_Cognitive_BCI/Multi-modal_Awareness_Status_Monitoring/BCI_generator.py
@@ -25,3 +25,23 @@ class BCIDataGenerator:
                 self.data.append(self.data_source['data'][f'{six:02}']['ppg'])
             stimulus_info = self.data_source['stimulus_info'][f'{six:02}']
             x = []
+            for tix in range(stimulus_info['ppg'].shape[0]):
+                p1 = 0 if tix is 0 else stimulus_info['ppg'][tix - 1]
+                p2 = stimulus_info['ppg'][tix]
+                # range(p1, p2-ppg_window,ppg_shift) is a list that contains points of ppg from p1 to p2. step is shift(default=2)sec.
+                # map change elements in range to tuple of (dix, next dix). list change tuple to list. np.stack put them in one list
+                x.append(
+                    np.stack(list(map(lambda dix: (dix, dix + ppg_window), range(p1, p2 - ppg_window, ppg_shift)))))
+                # return string that shows whether sbj responsed(0) or not(1).=>'00000110111...'
+            resp_str = ''.join(map(str, np.isinf(stimulus_info['resp']).astype(int)))
+            lab = np.full((len(x)), np.nan)  # return array of length len(x), filled with nan
+            # finditer finds index of string where 0 is repeated same or more than 3
+            for loc in re.finditer('0{3,}', resp_str):
+                lab[loc.start() + 1:loc.end()] = 0  # this lis labeled as 0, because it means awake.
+                # if 6 0 in row, change 5 of them except first one
+            for loc in re.finditer('1{3,}', resp_str):
+                lab[loc.start() + 1:loc.end()] = 1  # this is labeled as 1, because it means sleeping
+                # if 6 1 in row, change 5 of them except first one
+            for loc in re.finditer('000111', resp_str):
+                lab[loc.start() + 2:loc.end() - 1] = 2  # this is labeled as 2, because it means drowsy
+                # if 000111, it is changed to 002221
