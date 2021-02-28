31c02bc87c7244c5e9d34b5ca25c3b490d7bd166
rock <chomosi@gmail.com>
2021-02-28 23:54:20 +0900

    snn test

---
+++ 3_Cognitive_BCI/Multi-modal_Awareness_Status_Monitoring/snn_test.py
@@ -0,0 +1,10 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.layers import Flatten,  MaxPooling2D, Conv2D
+from keras.callbacks import TensorBoard
+
+
+#bindsnet library
+https://github.com/BindsNET/bindsnet
