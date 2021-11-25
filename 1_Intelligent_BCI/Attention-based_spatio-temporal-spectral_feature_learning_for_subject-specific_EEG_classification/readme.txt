135a8c8ca4d346960b5c18439419e4e3e04a3f03
DongHeeKo-KU <94820241+DongHeeKo-KU@users.noreply.github.com>
2021-11-25 15:17:45 +0900

    Delete 1_Intelligent_BCI/Attention-based_spatio-temporal-spectral_feature_learning_for_subject-specific_EEG_classification directory

--- 1_Intelligent_BCI/Attention-based_spatio-temporal-spectral_feature_learning_for_subject-specific_EEG_classification/readme.txt
+++
@@ -1,4 +0,0 @@
-* Programming Language: Python
-* Contact: Dong Hee Ko (donghee-ko@korea.ac.kr)
-* Function: This program is for motor imagery classification using CNN, RNN, attention mechanism.
-* Run: sh runs/train_all_subject.sh
--- 1_Intelligent_BCI/Attention-based_spatio-temporal-spectral_feature_learning_for_subject-specific_EEG_classification/runs/train_single_subject.sh
+++
@@ -1,1 +0,0 @@
-python main.py --summary --model=bci2021 --band=0,4,4,7,7,13,13,30,30,42 --labels=0,1 --start_time=-0.5 --end_time=4 --window_size=400 --step=50 -lr=1e-04 -sch=exp --gamma=0.993 --epochs=400 --batch_size=144 --save_dir=train --device=0
