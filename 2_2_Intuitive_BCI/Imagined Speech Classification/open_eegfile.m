00214cec61252082a388774ff08bab51101d76fa
2jungsun <114377663+2jungsun@users.noreply.github.com>
2022-10-24 16:45:33 +0900

    Update open_eegfile.m

--- 2_2_Intuitive_BCI/Imagined Speech Classification/open_eegfile.m
+++ 2_2_Intuitive_BCI/Imagined Speech Classification/open_eegfile.m
@@ -132,3 +132,10 @@ disp('All EEG Data Converting is Done!');
 % end
 
 % EEG_data = double(EEG_data);
+
+% clearvars -except EEG_data mnt mrk nfo dat
+
+
+% cd('D:\BTS_dataset\day3')
+
+% save(['raw_EEG_file.mat'],'-v7.3');
