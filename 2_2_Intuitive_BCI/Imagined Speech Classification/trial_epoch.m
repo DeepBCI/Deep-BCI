876a81acc0d572bf1fbe6f023d5373d8f9dd72ba
Young-Eun Lee -KU <95407049+deep-bci-g@users.noreply.github.com>
2022-10-28 17:53:05 +0900

    Update trial_epoch.m

--- 2_2_Intuitive_BCI/Imagined Speech Classification/trial_epoch.m
+++ 2_2_Intuitive_BCI/Imagined Speech Classification/trial_epoch.m
@@ -16,3 +16,9 @@ end
 EEG_data = double(EEG_data);
 
 clearvars -except EEG_data mnt mrk nfo dat
+
+concat_EEG = [];
+
+for i = 1:length(mrk.pos)
+    concat_EEG(:,:,i) = EEG_data(mrk.pos(1,i)-1499:mrk.pos(1,i),:); % time series x channel x trial
+end
