8d987430cbc21e430a0475f4c01a1838f6cbfa8e
jiwonLee-KU <114377302+jiwonLee-KU@users.noreply.github.com>
2022-11-28 11:20:29 +0900

    Add files via upload

---
+++ 2_2_Intuitive_BCI/Imagined Speech Classification/overt_sound_open.m
@@ -0,0 +1,62 @@
+clear all
+close all
+clc
+
+%% overt_soundfile
+
+cd('D:\BTS_dataset\2022_10_11_BTS_denseNet_waveNet\KSW2_KJW8\2_word\sub8\day3\sub8_jwkim_word_1');
+speech_file = dir('D:\BTS_dataset\2022_10_11_BTS_denseNet_waveNet\KSW2_KJW8\2_word\sub8\day3\sub8_jwkim_word_1\');
+speech_file = struct2table(speech_file);
+speech_filename = speech_file.name;
+
+except_trial = ("Weather"|"January"|"Conversation");
+delete_trial = strfind(speech_filename,except_trial);
+
+for i =1:length(speech_filename)
+    if any(delete_trial{i,1}) == 1
+        speech_filename{i,1} = [];
+    end
+end
+
+
+speech_filename{1,1} = [];
+speech_filename{2,1} = [];
+speech_filename{end,1} = [];
+
+O_n =1;
+for ex_o = 1:length(speech_filename)
+    if any(speech_filename{ex_o,1}) == 1
+        overt_filename{O_n,1} = speech_filename{ex_o,1};
+        O_n = O_n+1;
+    end
+end
+
+
+wordlist = {'Notebook','Apartment','Television'};
+for i = 1:length(wordlist)
+   overt_index = strfind(overt_filename,wordlist{1,i});
+   w_array = cell2mat(overt_index);
+   num_word(i,1) = length(w_array);
+   overt_index = [];
+end
+
+for i = 1:length(overt_filename)
+    tmp_overt_name{i,1} = 'xxx';
+end
+
+tmp_for_num_O = strcat(tmp_overt_name,overt_filename);
+
+for i = 1:1300
+    num_starting{i,1} = ['xxx',num2str(i),'_'];
+end
+
+list = 1;
+for i = 1:length(num_starting)
+    tmp_match = regexp(tmp_for_num_O,num_starting{i,1},'match');
+    find_match = find(~cellfun(@isempty,tmp_match));
+    if any(find_match) == 1
+         overt_filelist{list,1} = overt_filename{find_match,1};
+        list = list + 1;
+    end
+end
+
